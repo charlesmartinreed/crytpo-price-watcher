422e17785c84eb2d4508e7f6aa2e374a62b8c658
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-16 21:34:50 -0500

    Reorganize project. Add add.html. Add js files. Add css files.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
 const shell = require("electron").shell;
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -16,10 +17,10 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  win.loadFile("index.html");
+  win.loadFile(path.join(__dirname, "src/index.html"));
 
   // Open the DevTools.
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on("closed", () => {
